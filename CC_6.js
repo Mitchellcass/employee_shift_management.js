a4b4c0a498bf260dbe6c3c02029530561abe1c83
MitchCas <155246406+Mitchellcass@users.noreply.github.com>
2024-09-22 23:48:26 -0400

    Initialize employees with shift schedules

---
+++ CC_6.js
@@ -0,0 +1,10 @@
+//Task 1: Create an Employees Array of Employee Objects
+
+const employees = [
+    { name: 'Allison', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Wednesday', hours: 6 }] },
+    { name: 'Bill', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Thursday', hours: 7 }] },
+    { name: 'Clyde', shifts: [{ day: 'Monday', hours: 8 }] },
+    { name: 'Don', shifts: [{ day: 'Friday', hours: 8 }] }
+];
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # employee_shift_management.js
+
 This repo is for Coding Challenge 6
